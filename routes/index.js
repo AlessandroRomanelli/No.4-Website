f54d64662656835287a3d8a2394973e2ba42010e
Alessandro Romanelli <aleromrod@gmail.com>
2017-08-16 23:42:47 +0200

    Heroku fix

--- routes/index.js
+++ routes/index.js
@@ -16,47 +16,33 @@ var googleAuth = require('google-auth-library');
 var readFile = Promise.denodeify(require('fs').readFile);
 var unit = {};
 
-function authorize() {
-    return new Promise(resolve => {
-        const authFactory = new GoogleAuth();
-        const jwtClient = new authFactory.JWT(
-            process.env.GOOGLE_CLIENT_EMAIL, // defined in Heroku
-            null,
-            process.env.GOOGLE_PRIVATE_KEY, // defined in Heroku
-            ['https://www.googleapis.com/auth/spreadsheets.readonly']
-        );
+function authorize(credentials) {
+  debugger
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-        jwtClient.authorize(() => resolve(jwtClient));
-    });
+  // Check if we have previously stored a token.
+  debugger
+  return readFile(TOKEN_PATH).then(token => {
+    debugger
+    oauth2Client.credentials = JSON.parse(token);
+    return oauth2Client;
+  })
+  .catch(err => {
+    debugger
+    getNewToken(oauth2Client);
+  })
 }
-
-// function authorize(credentials) {
-//   debugger
-//   var clientSecret = credentials.installed.client_secret;
-//   var clientId = credentials.installed.client_id;
-//   var redirectUrl = credentials.installed.redirect_uris[0];
-//   var auth = new googleAuth();
-//   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-//
-//   // Check if we have previously stored a token.
-//   debugger
-//   return readFile(TOKEN_PATH).then(token => {
-//     debugger
-//     oauth2Client.credentials = JSON.parse(token);
-//     return oauth2Client;
-//   })
-//   .catch(err => {
-//     debugger
-//     getNewToken(oauth2Client);
-//   })
-// }
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
-// var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-// var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-//     process.env.USERPROFILE) + '/.credentials/';
-// var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -66,48 +52,47 @@ function authorize() {
  * @param {getEventsCallback} callback The callback to call with the authorized
  *     client.
  */
-// function getNewToken(oauth2Client) {
-//   debugger
-//   var authUrl = oauth2Client.generateAuthUrl({
-//     access_type: 'offline',
-//     scope: SCOPES
-//   });
-//   console.log('Authorize this app by visiting this url: ', authUrl);
-//   var rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   });
-//   rl.question('Enter the code from that page here: ', function(code) {
-//     rl.close();
-//     code = process.env.AUTH_CODE || code;
-//     return oauth2Client.getToken(code, function(err, token) {
-//       if (err) {
-//         console.log('Error while trying to retrieve access token', err);
-//         return err;
-//       }
-//       oauth2Client.credentials = token;
-//       storeToken(token);
-//       return oauth2Client;
-//     });
-//   });
-// }
+function getNewToken(oauth2Client) {
+  debugger
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    return oauth2Client.getToken(process.env.AUTH_CODE, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return err;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      return oauth2Client;
+    });
+  });
+}
 
 /**
  * Store token to disk be used in later program executions.
  *
  * @param {Object} token The token to store to disk.
  */
-// function storeToken(token) {
-//   try {
-//     fs.mkdirSync(TOKEN_DIR);
-//   } catch (err) {
-//     if (err.code != 'EEXIST') {
-//       throw err;
-//     }
-//   }
-//   fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-//   console.log('Token stored to ' + TOKEN_PATH);
-// }
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
 
 
 function getSheet(authClient) {
@@ -135,7 +120,11 @@ router.get('/', function(req, res, next) {
 router.get('/unit', function(req, res, next) {
   var weekdays=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
   // Load client secrets from a local file.
-  authorize()
+  readFile('client_secret.json')
+  .then(content => {
+    debugger
+    return authorize(JSON.parse(content));
+  })
   .then(authClient => {
     debugger
     var request = {
